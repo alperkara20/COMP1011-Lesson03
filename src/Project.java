96c7e53f549ad9824f73dbacf16c44334b5e3521
@alperkara20 <alperkara247@gmail.com>
2021-11-01 21:33:54 +0300

    Created abstract class "Vehicle" and implemented Truck and SportsUtilityVehicle classes. Overridden the methods in the subclasses of Vehicle.

--- src/Project.java
+++ src/Project.java
@@ -1,8 +1,4 @@
 /**
- * 
- */
-
-/**
  * @author Alper
  *
  */
@@ -12,8 +8,18 @@ public class Project {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		SportsUtilityVehicle tesla = new SportsUtilityVehicle("White", "Ford",	"FX", "2008");
+		Truck ford = new Truck(6,3,"White", "Ford",	"FX", "2008");
+		
+		for(int seconds = 0; seconds < 10; seconds ++) {
+
+			tesla.accelerate();
+			ford.accelerate();
+		}
+		
+		System.out.println("Vehicle Speed: " + tesla.getSpeed());
+		System.out.println("Truck Speed: " + ford.getSpeed());
 	}
 
 }
